5dec59cc1fc298f4d7c02f0ed9287ca890088e0d
cythia123 <Cynthiawangying123@163.com>
2019-08-01 14:52:11 +0800

    first commit

---
+++ vue_project/src/views/List.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <div class="header" v-if="isNone">还没有添加任何便签哦~~</div>
+    <div class="item-container" v-if="!isDetail">
+      <div class="item" v-for="(item,index) in pageLists" :key="index" @click="toggleItem(index)" :class="{'active' : index === activeIndex}">
+        <span class="delete" v-show="index==activeIndex" @click.stop="delItem(index)">&times;</span>
+        <p class="title">{{item.title}}<p>
+        <p class="content">{{item.content}}</p>
+        <div class="footer">
+          <div>{{item.time}}</div>
+          <a v-if="index === activeIndex" @click.stop="goDetail(index)">查看详情>></a>
+        </div>
+      </div>
+    </div>
+    <div class="detail" v-else>
+      <p class="title">{{detailTitle}}</p>
+      <p class="content">{{detailContent}}</p>
+      <button @click="backList()">返回</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import store from '@/store'
+  export default {
+//    name: 'List',
+    store,
+    data () {
+      return {
+        activeIndex:-1,
+        isDetail:false,
+        detailTitle: '',
+        detailContent: ''
+      }
+    },
+    mounted(){
+      store.commit('getLocal')
+    },
+    computed: {
+      pageLists (){
+        return store.getters.myLists
+      },
+      isNone () {
+        if (this.pageLists.length === 0) {
+          return true
+        }else {
+          return false
+        }
+      },
+    },
+    methods: {
+      toggleItem(index){
+        if(this.activeIndex === index) {
+          this.activeIndex = -1
+        }else{
+          this.activeIndex = index
+        }
+      },
+      delItem(index){
+        store.commit('delItem', index)
+        localStorage["pageLists"]=JSON.stringify(store.state.lists)//存到本地
+      },
+      goDetail(index){
+        this.isDetail=true
+        this.detailTitle=this.pageLists[index].title
+        this.detailContent=this.pageLists[index].content
+      },
+      backList (){
+        this.isDetail = false
+      }
+    },
+    // watch: {
+    //   pageLists: {
+    //     handler: function (newVal) {
+    //       localStorage["pageLists"]=JSON.stringify(newVal)
+    //     },
+    //     deep: true
+    //   }
+    // } // list组件不更新 更新是在add中完成 或者在app.vue中监听
+  }
+</script>
+<style scoped lang="scss">
+  .header{
+    text-align: center;
+    margin:5px;
+    font-size: 14px;
+  }
+  .item-container{
+    position: absolute;
+    top:0;
+    bottom:60px;
+    overflow: hidden;
+    width:100%;
+  }
+  .item{
+    position: relative;
+    border:1px solid #d5d5d5;
+    border-radius: 7px;
+    margin:10px;
+    font-size: 14px;
+    box-shadow: 2px 3px 3px #d5d5d5;
+    .title{
+      height:35px;
+      line-height: 25px;
+      padding:5px;
+      font-weight:bold;
+      border-bottom:1px solid #d5d5d5;
+    }
+    .content{
+      height:25px;
+      line-height: 25px;
+      padding:5px;
+      overflow: hidden;//超出部分隐藏
+      -ms-text-overflow: ellipsis;
+      text-overflow: ellipsis;//超出部分显示省略号
+      white-space: nowrap;//强制不换行，换行就不会超出了，也不会显示省略号
+    }
+    .footer{
+      height:20px;
+      display: flex;
+      justify-content: space-between;
+      font-size: 12px;
+      margin: 5px;
+      a {
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+    &.active{
+      color: #fff;
+      background: #42b983;
+    }
+    .delete{
+      position: absolute;
+      top:3px;
+      left:94%;
+      font-size: 20px;
+      cursor: pointer;
+      &:hover{
+        color: red;
+        font-weight:bold;
+        font-size: 22px;
+      }
+    }
+  }
+  .detail{
+    position: absolute;
+    top:0;
+    bottom:60px;
+    overflow: hidden;
+    width: 100%;
+    .title{
+      padding:10px;
+      font-weight:bold;
+      border-bottom: 1px solid #d5d5d5;
+    }
+    .content{
+      font-size:14px;
+      padding:15px;
+      letter-spacing:2px;
+      -ms-word-wrap: break-word;
+      word-wrap: break-word;
+    }
+    button{
+      width:90%;
+      margin-left:5%;
+      margin-top:30px;
+      height:35px;
+      line-height:35px;
+      border-radius: 7px;
+      border:1px solid #42b983;
+      background-color: #42b983;
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ vue_project/src/views/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="img"><img src="../assets/logo.png" alt=""></div>
+    <div class="user">{{userName}}</div>
+    <div class="welcome">欢迎来到个人中心</div>
+    <div class="add">
+      <router-link :to="{name: 'add'}">添加</router-link>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+
+  export default {
+    name: 'User',
+    data() {
+      return {
+        userName: sessionStorage.getItem('currentUser')
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .img{
+    text-align: center;
+  }
+  .user{
+    font-weight: bold;
+  }
+  .user,.welcome{
+    text-align: center;
+    margin: 5px;
+  }
+  .add{
+    text-align: center;
+    margin: 40px 20px 20px;
+    height:35px;
+    line-height:35px;
+    background-color: #42b983;
+    border:1px solid #42b983;
+    border-radius: 7px;
+    a{
+      text-decoration: none;
+      color: #fff;
+      display: block;
+    }
+  }
+</style>
